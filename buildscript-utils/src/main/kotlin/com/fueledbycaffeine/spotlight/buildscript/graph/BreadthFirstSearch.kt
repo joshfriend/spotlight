8a335d6ac97a44b6bce1d8ec4fe33e1d5a59b23b
Zac Sweers <pandanomic@gmail.com>
2025-05-19 21:34:02 -0400

    Optimize BFS (#14)

--- buildscript-utils/src/main/kotlin/com/fueledbycaffeine/spotlight/buildscript/graph/BreadthFirstSearch.kt
+++ buildscript-utils/src/main/kotlin/com/fueledbycaffeine/spotlight/buildscript/graph/BreadthFirstSearch.kt
@@ -3,21 +3,43 @@ package com.fueledbycaffeine.spotlight.buildscript.graph
 import java.util.*
 
 public object BreadthFirstSearch {
-  public fun <T: GraphNode<T>> flatten(nodes: Set<T>, rules: Set<ImplicitDependencyRule> = emptySet()): List<T> {
-    return run(nodes, rules).values.flatten().distinct()
+  // Initialize with generously sized capacities to avoid resizing as we grow
+  private const val INITIAL_CAPACITY = 4096
+
+  public fun <T : GraphNode<T>> flatten(nodes: Set<T>, rules: Set<ImplicitDependencyRule> = emptySet()): List<T> {
+    val deps = run(nodes, rules)
+    // In lieu of a flattenTo() option, this creates an intermediate
+    // set to avoid the intermediate list + distinct()
+    return buildList {
+      val seen = LinkedHashSet<T>(INITIAL_CAPACITY)
+      for (values in deps.values) {
+        for (dep in values) {
+          if (seen.add(dep)) {
+            add(dep)
+          }
+        }
+      }
+    }
   }
 
-  public fun <T: GraphNode<T>> run(nodes: Set<T>, rules: Set<ImplicitDependencyRule> = emptySet()): Map<T, Set<T>> {
-    val dependenciesMap = mutableMapOf<T, Set<T>>()
-    val queue = LinkedList<T>()
+  public fun <T : GraphNode<T>> run(nodes: Set<T>, rules: Set<ImplicitDependencyRule> = emptySet()): Map<T, Set<T>> {
+    // one set for all the visited bookkeeping
+    val seen = HashSet<T>(INITIAL_CAPACITY)
+    val dependenciesMap = LinkedHashMap<T, Set<T>>(INITIAL_CAPACITY)
+    val queue = ArrayDeque<T>(INITIAL_CAPACITY)
+
     queue.addAll(nodes)
+    seen += nodes
 
     while (queue.isNotEmpty()) {
-      val nextNode = queue.poll()
-      if (nextNode !in dependenciesMap) {
-        val successors = nextNode.findSuccessors(rules)
-        dependenciesMap[nextNode] = successors
-        queue.addAll(successors)
+      val nextNode = queue.removeFirst()
+      val successors = nextNode.findSuccessors(rules)
+      dependenciesMap[nextNode] = successors
+
+      for (successor in successors) {
+        if (seen.add(successor)) {
+          queue.addLast(successor)
+        }
       }
     }
 
