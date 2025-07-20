2d2b929d9c40e52a24d51d8fce4e6c4100708064
Zac Sweers <pandanomic@gmail.com>
2025-07-19 20:18:46 -0400

    Cache the ruleSetAdapter

--- buildscript-utils/src/main/kotlin/com/fueledbycaffeine/spotlight/buildscript/SpotlightRulesList.kt
+++ buildscript-utils/src/main/kotlin/com/fueledbycaffeine/spotlight/buildscript/SpotlightRulesList.kt
@@ -4,13 +4,13 @@ import com.fueledbycaffeine.spotlight.buildscript.graph.ImplicitDependencyRule
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.ToJson
-import com.squareup.moshi.Types.newParameterizedType
+import com.squareup.moshi.adapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dev.zacsweers.moshix.sealed.reflect.MoshiSealedJsonAdapterFactory
-import okio.IOException
 import java.nio.file.Path
 import kotlin.io.path.exists
 import kotlin.io.path.readText
+import okio.IOException
 
 public class SpotlightRulesList(private val root: Path) {
   public companion object {
@@ -18,12 +18,10 @@ public class SpotlightRulesList(private val root: Path) {
   }
 
   public fun read(): Set<ImplicitDependencyRule> {
-    val type = newParameterizedType(Set::class.java, ImplicitDependencyRule::class.java)
-    val adapter = moshi.adapter<Set<ImplicitDependencyRule>>(type)
     val rulesPath = root.resolve(SPOTLIGHT_RULES_LOCATION)
     return if (rulesPath.exists()) {
       try {
-        adapter.fromJson(rulesPath.readText()) ?: emptySet()
+        ruleSetAdapter.fromJson(rulesPath.readText()) ?: emptySet()
       } catch (e: IOException) {
         throw InvalidSpotlightRules("Spotlight rules at $SPOTLIGHT_RULES_LOCATION were invalid", e)
       }
@@ -39,6 +37,9 @@ public class SpotlightRulesList(private val root: Path) {
       .addLast(KotlinJsonAdapterFactory())
       .build()
   }
+
+  @OptIn(ExperimentalStdlibApi::class) // Not actually experimental anymore!
+  private val ruleSetAdapter = moshi.adapter<Set<ImplicitDependencyRule>>()
 }
 
 public class InvalidSpotlightRules(message: String, cause: Throwable) : Exception(message, cause)
