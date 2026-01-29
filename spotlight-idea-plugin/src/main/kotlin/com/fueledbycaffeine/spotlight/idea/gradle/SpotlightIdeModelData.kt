22bf4aabb53d71fd652ac3189db8b10e1e4f7d34
Josh Friend <josh@fueledbycaffeine.com>
2026-01-29 07:52:20 -0500

    Fix SerializationException when saving external project data
    Convert Gradle Tooling API proxy to concrete data class before storing
    in DataNode, since IntelliJ cannot serialize JDK dynamic proxies.

--- spotlight-idea-plugin/src/main/kotlin/com/fueledbycaffeine/spotlight/idea/gradle/SpotlightIdeModelData.kt
+++ spotlight-idea-plugin/src/main/kotlin/com/fueledbycaffeine/spotlight/idea/gradle/SpotlightIdeModelData.kt
@@ -1,5 +1,6 @@
 package com.fueledbycaffeine.spotlight.idea.gradle
 
+import com.fueledbycaffeine.spotlight.buildscript.models.SpotlightModel
 import com.intellij.openapi.externalSystem.model.Key
 import com.intellij.openapi.externalSystem.model.ProjectKeys
 import java.io.Serializable
@@ -10,17 +11,17 @@ import java.io.Serializable
  * The presence of this data node indicates that Spotlight is enabled.
  * The absence means Spotlight is disabled (plugin not applied).
  */
-data class SpotlightModelData(
+data class SpotlightIdeModelData(
   /**
    * Set of Gradle project paths that are included in the build.
    * Format: Gradle path strings (e.g., ":", ":app", ":libraries:core")
    */
-  val includedProjectPaths: Set<String>
-) : Serializable {
+  override val includedProjectPaths: Set<String>
+) : SpotlightModel, Serializable {
   companion object {
     @JvmField
-    val KEY: Key<SpotlightModelData> = Key.create(
-      SpotlightModelData::class.java,
+    val KEY: Key<SpotlightIdeModelData> = Key.create(
+      SpotlightIdeModelData::class.java,
       ProjectKeys.PROJECT.processingWeight + 2
     )
   }
